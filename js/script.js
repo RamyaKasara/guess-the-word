e9d124abd42c6f191dd9dfe5f78d64cc4607987d
ramya_k <ramya.kasarabada@gmail.com>
2022-02-06 14:46:02 +0530

    Added code to display guessed, check if guess is correct and if user has won

--- js/script.js
+++ js/script.js
@@ -20,25 +20,25 @@ const guessedLetters =[];
 const word = "magnolia";
 
 //function to update word guessing progress
-const updateWordInProgress = function(){
+const initialiseWordInProgress = function(){
     const len = word.length;
     wordInProgress.innerText = "";
     for(i = 0; i< len; i++){
-        wordInProgress.innerText = wordInProgress.innerText + " ● ";
+        wordInProgress.innerText = wordInProgress.innerText + "●";
     }
 };
 
-updateWordInProgress(word);
+initialiseWordInProgress(word);
 
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
     let val = letter.value;
     //console.log(val);
     letter.value = "";//clear the input space
-    message.innerText = "";
-    let validatedInput = validateLetter(val);
+    message.innerText = "";// clear message 
+    let validatedInput = validateLetter(val);// make sure it is a letter
     if(validatedInput!== undefined){
-        makeGuess(validatedInput);
+        makeGuess(validatedInput);// add to guess list if not already present
     }
 });
 
@@ -61,7 +61,55 @@ const makeGuess = function(letter){
         message.innerText = "You already guessed that letter, silly. Try again.";
     else{
         guessedLetters.push(letter.toUpperCase());
-        console.log(guessedLetters);
+        //console.log(guessedLetters);
+        updateGuessList(); // add alphabet if not already present and display in the guessed list of alphabets
+        updateWordInProgress(guessedLetters) // check if alphabet entered is there in the word
+        winOrNot();// check if user has guessed the complete word
     }
 };
 
+//PART 3
+const updateGuessList = function(){
+    guessList.innerHTML = "";
+    for(let alphabet of guessedLetters){
+        const letterItem = document.createElement("li");
+        letterItem.innerText = alphabet;
+        guessList.append(letterItem);
+    }
+};
+
+const updateWordInProgress = function(guessedLetters){
+    wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    //console.log(wordArray);
+    const wordProgress = wordInProgress.innerText.split("");
+    //creates array as ['●', '●', '●', '●', '●', '●', '●', '●']
+    //console.log(wordProgress);
+    //console.log(wordInProgress.innerText);
+    guessedLetters.forEach(function(alphabet,index) {
+        //for each alphabet guessed, if word Array has it,
+        if(wordArray.includes(alphabet)){
+            //find every occurance of alphabet  
+            for(i = 0; i< wordArray.length; i++){
+                if(wordArray[i] === alphabet){
+                    //replace every corresponding circle with the alphabet
+                    wordProgress[i] = alphabet;
+                }
+                    
+            }
+        }
+        
+    });
+
+    wordInProgress.innerText = wordProgress.join("");
+    //join(separator to be used while returning string)
+    //returns "MAGNOLIA"
+};
+
+
+const winOrNot = function(){
+    if(wordInProgress.innerText === word.toUpperCase()){
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }
+};
